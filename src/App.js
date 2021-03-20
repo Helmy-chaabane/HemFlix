5e61059788acb19bb800c0372ce6ebc262f74b80
Helmy-chaabane <Chaabane504@gmail.com>
2021-03-20 18:58:24 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Switch } from "react-router";
 import Login from "./Pages/Login/Login";
-import Main from "./Components/MainContent/Main";
+import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
 import Acceuil from "./Pages/Acceuil/Acceuil";
 import Movie from "./Pages/Movie/Movie";
 import MyMovies from "./Pages/MyMovies/MyMovies";
@@ -11,34 +11,10 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route
-          path="/movie/:id"
-          exact
-          render={(props) => {
-            return <Main component={Movie} {...props} />;
-          }}
-        />
-        <Route
-          path="/mymovies"
-          exact
-          render={(props) => {
-            return <Main component={MyMovies} {...props} />;
-          }}
-        />
-        <Route
-          path="/"
-          exact
-          render={(props) => {
-            return <Main component={Acceuil} {...props} />;
-          }}
-        />
-        <Route
-          path="/actors"
-          exact
-          render={(props) => {
-            return <Main component={Actors} {...props} />;
-          }}
-        />
+        <ProtectedRoute path="/" exact component={Acceuil} />
+        <ProtectedRoute path="/mymovies" exact component={MyMovies} />
+        <ProtectedRoute path="/movie/:id" exact component={Movie} />
+        <ProtectedRoute path="/actors" exact component={Actors} />
         <Route path="/login" exact component={Login} />
       </Switch>
     </div>
